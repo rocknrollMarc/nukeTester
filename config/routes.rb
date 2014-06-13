113a30059cabbc6e9f299d83b1d8f8ab7d15edf2
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-06-13 11:54:00 +0200

    devise register and login working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
+  resources :users
   resources :projcts
 
   # The priority is based upon order of creation: first created -> highest priority.
